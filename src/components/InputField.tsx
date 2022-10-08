8caa6340c48dc87230dc51836e86e712a24a7be4
Dmitry Velichko <soleil-2005@mail.ru>
2022-10-08 10:55:09 +0400

    new commit

---
+++ src/components/InputField.tsx
@@ -0,0 +1,19 @@
+import './styles.css'
+
+
+const InputField = () => {
+  return (
+    <form className='input'>
+      <input
+        type='input'
+        placeholder='Enter a task'
+        className='input__box'
+      ></input>
+      <button type='submit' className='input__submit'>
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
